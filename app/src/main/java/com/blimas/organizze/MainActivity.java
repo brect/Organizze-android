84ece0b4cfadb8eef7dccf383eaf6fb5c71373d4
brect <b.limas93@gmail.com>
2020-01-27 20:01:41 -0300

    Fragments criados e adicionados na mainactivity

--- app/src/main/java/com/blimas/organizze/MainActivity.java
+++ app/src/main/java/com/blimas/organizze/MainActivity.java
@@ -1,17 +1,54 @@
 package com.blimas.organizze;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide
+                .Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .canGoBackward(false)
+                .build());
+
+        addSlide(new FragmentSlide
+                .Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide
+                .Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide(new FragmentSlide
+                .Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
+
+        addSlide(new FragmentSlide
+                .Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.into_cadastro)
+                .canGoForward(false)
+                .build());
+
     }
 
 
-    //CRIADO
 }
