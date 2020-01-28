a294eebaafa046dfe99e99f0fca829f6a8376a1b
brect <b.limas93@gmail.com>
2020-01-28 00:10:05 -0300

    Criacao tela de cadastro e tela de login;

--- app/src/main/java/com/blimas/organizze/MainActivity.java
+++ app/src/main/java/com/blimas/organizze/MainActivity.java
@@ -1,18 +1,37 @@
 package com.blimas.organizze;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.blimas.organizze.activity.CadastroActivity;
+import com.blimas.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
 
+
+    private TextView btnLogar;
+    private Button btnCadastrar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        
+        btnLogar = findViewById(R.id.btnLogar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        configuraFragmentsSlide();
 
 
+    }
+
+    private void configuraFragmentsSlide() {
         setButtonBackVisible(false);
         setButtonNextVisible(false);
 
@@ -50,5 +69,15 @@ public class MainActivity extends IntroActivity {
 
     }
 
+
+//    public void efetuarCadastro(){
+//        Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
+//        startActivity(intent);
+//    }
+
+//    public void efetuarLogin(){
+//        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//        startActivity(intent);
+//    }
 
 }
